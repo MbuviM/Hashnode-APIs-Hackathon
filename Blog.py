9287936f662078456051cc0962eff91776a4bd9e
MbuviM <mwendembuvi10@gmail.com>
2024-01-25 18:47:23 +0300

    Python file

--- Blog.py
+++ Blog.py
@@ -1,15 +1,20 @@
 import requests
 from gql import Client, gql
+import os
 
+hashnode_url = "https://gql.hashnode.com"
+# The headers variable is used to store the authentication token and alert the API of the format data will be sent.
 
-
-def publish_blog(PublishPostInput, Hashnode_API_key):
-    url = "https://gql.hashnode.com"
-    Hashnode_API_key = "PAT"
-
-    #Graphql Mutation for publishing a new post
+api_auth_key = os.environ.get('HASHNODE_API_KEY')
+if api_auth_key is not None:
     
-    mutation = """
+    auth_headers = {"Authorization": f"{api_auth_key}"}
+
+else:
+    print("Error: HASHNODE_API_KEY is not set.")
+ 
+#Graphql Mutation for publishing a new post
+post_mutation = """
     PublishPost($input: PublishPostInput!) {
         publishPost(input: $input) {
     post {
@@ -86,5 +91,31 @@ def publish_blog(PublishPostInput, Hashnode_API_key):
   }
 }
 """
+# The input variables for the query
+PublishPostInput = {
+    "user_id" : 'id',
+    "slug" : 'slug',
+    "title" : 'Blog Title',
+    "subtitle" : 'Blog Subtitle',
+    "author" : "Author"
+}
 
+# Variables for the Graphql Mutation
+input_variables = {"input": PublishPostInput}
 
+#Graphql Payload
+request_payload = {
+    "query": post_mutation,
+    "variables" : input_variables
+}
+
+# Graphql Request
+def make_graphql_request(url, headers, json):
+  response = requests.post(url=hashnode_url, headers=auth_headers, json=request_payload)
+
+  if response.status_code == 200:
+    print("Blog posted successfully!")
+  else:
+    print("An error occurred. Kindly check on it and try again!")
+
+  return response
